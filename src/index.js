c62e79d86137429ea4ef15ebcd120dc0176a0048
Orlando Cabral <orlandocabral@live.com>
2023-03-24 17:10:48 -0500

    feat: change default port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // process.ENV.PORT || 3000;
+const port = process.ENV.PORT || 3000;
 const routerApi = require('./routes/main.routes');
 
 app.use(express.json());
